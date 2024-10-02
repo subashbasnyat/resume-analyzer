c05d4aa39e6d105d237bc90b7de23edef3c0f9df
Restyled.io <commits@restyled.io>
2024-10-02 15:26:58 +0000

    Restyled by reorder-python-imports

--- resume_analyzer/preprocessing.py
+++ resume_analyzer/preprocessing.py
@@ -1,6 +1,7 @@
 import nltk
 from nltk.corpus import stopwords
-from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
 
 
 class TextCleaner:
